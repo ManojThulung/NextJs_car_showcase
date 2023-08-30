022ed860fd27f0dcafbe2d51de41a6d0533bc5d7
manoj <manojthulung03@gmail.com>
2023-08-30 17:34:20 +0545

    added header, hero section and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScrolll = () => {};
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car - quickly and easilty!
+        </h1>
+        <p className="hero_subtitle">
+          Streamline your car rental experience with our effortless booking
+          process
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScrolll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="car image"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="hero__image-overlay" />
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
