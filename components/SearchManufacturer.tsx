69cae965e26c0d549f2834bd9ae7b84e68ae1781
manoj <manojthulung03@gmail.com>
2023-08-31 19:52:10 +0545

    added combobox from headless ui

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
+import { SearchManufacturerProps } from "@/types";
+import { manufacturers } from "@/constants";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [value, setValue] = useState("");
+
+  const filteredManufacturers =
+    value === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(value.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox value={manufacturer} onChange={setManufacturer}>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Brand Name"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setValue("")}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    } `
+                  }
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
